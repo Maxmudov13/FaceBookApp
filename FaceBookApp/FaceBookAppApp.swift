67ff246068536d2f2393dc7fad0a158fd4bfaf34
iMtRo <maxmudovilyos75@gmail.com>
2022-01-19 06:13:50 +0500

    Initial Commit

---
+++ FaceBookApp/FaceBookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceBookAppApp.swift
+//  FaceBookApp
+//
+//  Created by user on 19/01/22.
+//
+
+import SwiftUI
+
+@main
+struct FaceBookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
